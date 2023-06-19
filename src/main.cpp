44f821490086f99f79d6c2b64bb77a14a7d8a43f
Sameen Karim <sameenkarim@gmail.com>
2023-06-19 17:09:51 -0400

    create software serial for driver uart

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,33 @@
  * Powered by a TMC2209 stepper driver
  */
 
-#include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <TMCStepper.h>
-#include <AccelStepper.h>
 
 #define LED_ESP_PIN 2
 #define LED_MCU_PIN 16
 
-#define EN_PIN    10  // Driver enable pin
-#define DIR_PIN   4   // Driver irection pin
+#define EN_PIN    14  // Driver enable pin
+#define DIR_PIN   4   // Driver direction pin
 #define STEP_PIN  5   // Driver step pin
 
 #define SW_SCK          5     // Clock
-#define SW_TX           1     // Serial receive pin
-#define SW_RX           3     // Serial transmit pin
+#define SW_TX           9     // Serial receive pin
+#define SW_RX           10    // Serial transmit pin
 #define DRIVER_ADDRESS  0b00  // TMC2209 Driver address according to MS1 and MS2
 #define R_SENSE         0.11f // SilentStepStick series use 0.11
 
 #define STEP_DELAY    1000  // delay between steps in microseconds
 #define STEPS_PER_REV 900   // number of steps per revolution
+#define MOTOR_CURRENT 2800  // motor current in milliamps
+#define MICROSTEPS    8     // microsteps per step
 
-TMC2209Stepper driver(&Serial, R_SENSE, DRIVER_ADDRESS);
+#define SERIAL_BAUD_RATE    115200
+#define SW_SERIAL_BAUD_RATE 9600
+
+SoftwareSerial SWSerial(SW_RX, SW_TX);
+
+TMC2209Stepper driver(&SWSerial, R_SENSE, DRIVER_ADDRESS);
 
 // spin direction
 bool dir = false;
@@ -42,8 +48,11 @@ void moveStepper(int steps, int dir) {
 }
 
 void setup() {
-  Serial.begin(115200);         // initialize hardware serial for UART motor control
-  driver.beginSerial(115200);   // Initialize UART
+  Serial.begin(SERIAL_BAUD_RATE);           // initialize hardware serial for logging
+  SWSerial.begin(SW_SERIAL_BAUD_RATE);      // initialize software serial for UART motor control
+  driver.beginSerial(SW_SERIAL_BAUD_RATE);  // Initialize driver over UART
+
+  Serial.println("init start");
   
   // Set pinmodes
   pinMode(EN_PIN, OUTPUT);
@@ -56,22 +65,27 @@ void setup() {
   digitalWrite(EN_PIN, LOW);
 
   driver.begin();
-  driver.rms_current(2800); // Set motor RMS current
-  driver.microsteps(0);     // Set microsteps
+  driver.rms_current(MOTOR_CURRENT); // Set motor RMS current
+  driver.microsteps(MICROSTEPS);     // Set microsteps
+
+  Serial.print("current set to:");
+  Serial.println(driver.rms_current());
 
   // use stealthChop for quiet operation
   driver.toff(3);
-  driver.en_spreadCycle(false);
+  driver.en_spreadCycle(true);
   driver.pwm_autoscale(true);
 }
 
 void loop() {
+  Serial.println("spin forward");
   digitalWrite(LED_ESP_PIN, HIGH);  // turn off esp light
   digitalWrite(LED_MCU_PIN, LOW);   // turn on mcu light
   moveStepper(STEPS_PER_REV, 1);
 
   delay(1000); // pause for a second
 
+  Serial.println("spin backward");
   digitalWrite(LED_ESP_PIN, LOW);   // turn on esp light
   digitalWrite(LED_MCU_PIN, HIGH);  // turn off light
   moveStepper(STEPS_PER_REV, 0);
