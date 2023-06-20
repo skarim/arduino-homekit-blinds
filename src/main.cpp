f2003b9b96956eb3e933869822a0ba2fe0a75994
Sameen Karim <sameenkarim@gmail.com>
2023-06-19 22:48:03 -0400

    fix crashes from too many continuous steps

--- src/main.cpp
+++ src/main.cpp
@@ -21,30 +21,39 @@
 #define R_SENSE         0.11f // SilentStepStick series use 0.11
 
 #define STEP_DELAY    1000  // delay between steps in microseconds
-#define STEPS_PER_REV 900   // number of steps per revolution
+#define STEPS_PER_REV 1600  // number of steps per revolution
 #define MOTOR_CURRENT 2800  // motor current in milliamps
 #define MICROSTEPS    8     // microsteps per step
+#define MAX_CONTINUOUS_STEPS  STEPS_PER_REV/2
 
 #define SERIAL_BAUD_RATE    115200
-#define SW_SERIAL_BAUD_RATE 9600
+#define SW_SERIAL_BAUD_RATE 19200
 
 SoftwareSerial SWSerial(SW_RX, SW_TX);
 
 TMC2209Stepper driver(&SWSerial, R_SENSE, DRIVER_ADDRESS);
 
-// spin direction
-bool dir = false;
-
 void moveStepper(int steps, int dir) {
+  int currentSteps = min(steps, MAX_CONTINUOUS_STEPS);
+  Serial.print("moving ");
+  Serial.println(currentSteps);
+
   digitalWrite(EN_PIN, LOW); // enable stepper motor driver
   digitalWrite(DIR_PIN, dir);
-  for (int i = 0; i < steps; i++) {
+  for (int i = 0; i < currentSteps; i++) {
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(STEP_DELAY);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(STEP_DELAY);
   }
   digitalWrite(EN_PIN, HIGH); // disable stepper motor driver
+
+  int additionalSteps = steps - currentSteps;
+  Serial.print("remaining steps: ");
+  Serial.println(additionalSteps);
+  if (additionalSteps > 0) {
+    moveStepper(additionalSteps, dir);
+  }
 }
 
 void setup() {
@@ -61,34 +70,40 @@ void setup() {
   pinMode(LED_ESP_PIN, OUTPUT);
   pinMode(LED_MCU_PIN, OUTPUT);
 
-  // Enable driver board
+  // Enable driver
   digitalWrite(EN_PIN, LOW);
 
   driver.begin();
-  driver.rms_current(MOTOR_CURRENT); // Set motor RMS current
-  driver.microsteps(MICROSTEPS);     // Set microsteps
+  driver.rms_current(MOTOR_CURRENT);  // set motor RMS current
+  driver.microsteps(MICROSTEPS);      // set microsteps
+  driver.blank_time(24);              // driver blanking time
+  driver.irun(31);                    // motor run current
+  driver.TCOOLTHRS(0xFFFFF);          // coolstep threshold
+  driver.SGTHRS(255);                 // stallguard threshold
 
   Serial.print("current set to:");
   Serial.println(driver.rms_current());
 
   // use stealthChop for quiet operation
   driver.toff(3);
-  driver.en_spreadCycle(true);
+  driver.en_spreadCycle(false);
   driver.pwm_autoscale(true);
 }
 
 void loop() {
+  uint16_t msread=driver.microsteps();
+  Serial.print(F("Read microsteps via UART to test UART receive : "));
+  Serial.println(msread);
+
   Serial.println("spin forward");
   digitalWrite(LED_ESP_PIN, HIGH);  // turn off esp light
   digitalWrite(LED_MCU_PIN, LOW);   // turn on mcu light
-  moveStepper(STEPS_PER_REV, 1);
-
+  moveStepper(STEPS_PER_REV*3, 1);
   delay(1000); // pause for a second
 
   Serial.println("spin backward");
   digitalWrite(LED_ESP_PIN, LOW);   // turn on esp light
   digitalWrite(LED_MCU_PIN, HIGH);  // turn off light
-  moveStepper(STEPS_PER_REV, 0);
-
+  moveStepper(STEPS_PER_REV*3, 0);
   delay(1000); // pause for a second
 }
