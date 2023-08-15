7039e1e979bdc2c3d7c86c14a80833b8cd904ee5
Sameen Karim <sameenkarim@gmail.com>
2023-08-14 20:58:57 -0400

    move to esp32 board

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,36 @@
 /**
- * ESP 8266 smart blinds
+ * ESP32 smart blinds
  * Using a stepper motor
  * Powered by a TMC2209 stepper driver
  */
 
 #include <EEPROM.h>
-#include <SoftwareSerial.h>
 #include <TMCStepper.h>
 #include <ESPAsyncWebServer.h>
 #include <Config.h>
 
-#define LED_ESP_PIN 2
-#define LED_MCU_PIN 16
+#define LED_PIN 2
 
-#define EN_PIN    14  // Driver enable pin
-#define DIR_PIN   4   // Driver direction pin
-#define STEP_PIN  5   // Driver step pin
+#define SERIAL_PORT Serial2
 
-#define SW_SCK          5     // Clock
-#define SW_TX           9     // Serial receive pin
-#define SW_RX           10    // Serial transmit pin
+#define EN_PIN    25  // Driver enable pin
+#define DIR_PIN   26  // Driver direction pin
+#define STEP_PIN  27  // Driver step pin
+
+#define CLK             14    // Clock
 #define DRIVER_ADDRESS  0b00  // TMC2209 Driver address according to MS1 and MS2
 #define R_SENSE         0.11f // SilentStepStick series use 0.11
 
-#define STEP_DELAY    2000  // delay between steps in microseconds
+#define STEPS_DELAY_FAST  750   // delay between steps in microseconds
+#define STEPS_DELAY_SLOW  1000  // delay between steps in microseconds
+
 #define STEPS_PER_REV 1600  // number of steps per revolution
 #define MOTOR_CURRENT 2800  // motor current in milliamps
 #define MICROSTEPS    256   // microsteps per step
-#define MAX_CONTINUOUS_STEPS  STEPS_PER_REV/4
 
 #define SERIAL_BAUD_RATE    115200
-#define SW_SERIAL_BAUD_RATE 19200
 
-SoftwareSerial SWSerial(SW_RX, SW_TX);
-TMC2209Stepper driver(&SWSerial, R_SENSE, DRIVER_ADDRESS);
+TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
 AsyncWebServer server(80);
 
 // blinds settings
@@ -47,47 +44,37 @@ bool spinning = false;
 int direction; // 1 is closing, -1 is opening
 
 void moveStepper(int steps, int dir) {
-  int currentSteps = min(steps, MAX_CONTINUOUS_STEPS);
+  digitalWrite(LED_PIN, HIGH); // turn on light
   Serial.print("moving ");
-  Serial.println(currentSteps);
+  Serial.println(steps);
 
-  digitalWrite(LED_ESP_PIN, LOW); // turn on esp light
+  int stepsDelay = dir ? STEPS_DELAY_FAST : STEPS_DELAY_SLOW;
+
   digitalWrite(EN_PIN, LOW); // enable stepper motor driver
   digitalWrite(DIR_PIN, dir);
-  for (int i = 0; i < currentSteps; i++) {
+  for (int i = 0; i < steps; i++) {
     digitalWrite(STEP_PIN, HIGH);
-    // yield();
-    delayMicroseconds(STEP_DELAY);
+    delayMicroseconds(stepsDelay);
     digitalWrite(STEP_PIN, LOW);
-    // yield();
-    delayMicroseconds(STEP_DELAY);
+    delayMicroseconds(stepsDelay);
   }
   digitalWrite(EN_PIN, HIGH); // disable stepper motor driver
-  digitalWrite(LED_ESP_PIN, HIGH); // turn off esp light
-
-  int additionalSteps = steps - currentSteps;
-  Serial.print("remaining steps: ");
-  Serial.println(additionalSteps);
-  if (additionalSteps > 0) {
-    moveStepper(additionalSteps, dir);
-  }
+  digitalWrite(LED_PIN, LOW); // turn off light
 }
 
 void runServer() {
   // get position
   server.on("/position", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    digitalWrite(LED_MCU_PIN, LOW);
     request->send(200, "text/plain", String(currentPosition));
-    digitalWrite(LED_MCU_PIN, HIGH);
   });
 
   server.begin();
 }
 
 void setup() {
-  Serial.begin(SERIAL_BAUD_RATE);           // initialize hardware serial for logging
-  SWSerial.begin(SW_SERIAL_BAUD_RATE);      // initialize software serial for UART motor control
-  driver.beginSerial(SW_SERIAL_BAUD_RATE);  // Initialize driver over UART
+  Serial.begin(SERIAL_BAUD_RATE);           // initialize serial0 for logging
+  Serial2.begin(SERIAL_BAUD_RATE);          // initialize serial2 for UART motor control
+  driver.begin();                           // Initialize driver over UART
 
   Serial.println("init start");
   
@@ -95,12 +82,10 @@ void setup() {
   pinMode(EN_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
-  pinMode(LED_ESP_PIN, OUTPUT);
-  pinMode(LED_MCU_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   // Turn off lights
-  digitalWrite(LED_ESP_PIN, HIGH);
-  digitalWrite(LED_MCU_PIN, HIGH);
+  digitalWrite(LED_PIN, LOW);
 
   // Enable driver
   digitalWrite(EN_PIN, LOW);
@@ -137,23 +122,19 @@ void setup() {
 }
 
 void loop() {
-  uint16_t msread=driver.microsteps();
+  uint16_t msread = driver.microsteps();
   Serial.print(F("Read microsteps via UART to test UART receive : "));
   Serial.println(msread);
 
-  Serial.println("spin forward");
-  digitalWrite(LED_ESP_PIN, LOW);  // turn on esp light
-  digitalWrite(LED_MCU_PIN, LOW);   // turn on mcu light
+  Serial.println("spin forward - closing"); // closing
   digitalWrite(EN_PIN, LOW);
   moveStepper(STEPS_PER_REV*3, 1);
   digitalWrite(EN_PIN, HIGH);
-  delay(5000); // pause for a second
+  delay(2000); // pause for 2s
 
-  Serial.println("spin backward");
-  digitalWrite(LED_ESP_PIN, LOW);   // turn on esp light
-  digitalWrite(LED_MCU_PIN, HIGH);  // turn off light
+  Serial.println("spin backward - opening"); // opening
   digitalWrite(EN_PIN, LOW);
   moveStepper(STEPS_PER_REV*3, 0);
   digitalWrite(EN_PIN, HIGH);
-  delay(5000); // pause for a second
+  delay(2000); // pause for 2s
 }
